5e5f5e40347d921a5407bbe326332ac8c4b00e78
Byron Bos <byron.bos@gmail.com>
2022-10-22 14:51:03 +1300

    Updated template

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function IndexPage() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-red-500 text-ellipsis text-xxl">Hello World</h1>
       <a href="/api/users">View Users</a>
     </div>
   );
